5a0c2dde9942ec56a3fdeab095276cd8eaf823e7
Earl Nino Roma <roma.earl@gmail.com>
2017-02-25 00:13:50 +0800

    First commit

---
+++ src/app/components/user.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import {PostsService} from '../services/posts.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'user',
+  templateUrl: 'user.component.html',
+  providers: [PostsService]
+})
+export class UserComponent  { 
+  name: string;
+  email: string;
+  address: address;
+  hobbies: string[];
+  showHobbies: boolean; 
+  posts: Post[];
+
+constructor(private PostsService: PostsService){
+  this.name = 'John Doe';
+  this.email = 'john@gmail.com';
+  this.address = {
+      street: '12 Main st',
+      city: 'Boston',
+      state: 'MA'
+  };
+  this.hobbies = ['Music', 'Movies', 'Sports'];
+  this.showHobbies = false;
+
+  this.PostsService.getPosts().subscribe(posts => {
+        this.posts = posts;
+  });
+
+}
+
+    toggleHobbies(){
+        if(this.showHobbies == true){
+            this.showHobbies = false;
+        }else{
+            this.showHobbies = true;
+        }
+        
+        console.log("show");
+    }
+
+    addHobby(hobby:string){
+        console.log(hobby);
+        this.hobbies.push(hobby);
+    }
+
+    deleteHobby(i:number){
+        this.hobbies.splice(i, 1);
+    }
+
+}
+
+interface address{
+    street: string;
+    city: string;
+    state: string;
+}
+
+interface Post{
+    id: number;
+    title: string;
+    body: string;
+}
+
+
+
+
+
